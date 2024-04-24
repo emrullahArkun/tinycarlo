1efb516e2ae59655ea07e48d8f3b77e500c57634
Daniel Riege <daniel.riege99@gmail.com>
2024-04-24 16:22:11 +0200

    some changes

--- examples/stanley_control.py
+++ examples/stanley_control.py
@@ -14,20 +14,20 @@ config = {
         "real_world_env": "autosys"
     },
     "car": {
-        "wheelbase": 0.065,#0.0487, # distance between front and rear axle in meters
+        "wheelbase": 0.0487,#0.065,#0.0487, # distance between front and rear axle in meters
         "track_width": 0.027, # distance between the left and right wheel in meters
         "max_velocity": 0.15, # in m/s
         "max_steering_angle": 30, # in degrees
         "steering_speed": 30, # in deg/s
         "max_acceleration": 0.1, # in m/s^2
         "max_deceleration": 1, # in m/s^2
-        "tinycar_hostname": "192.168.84.49",
+        "tinycar_hostname": "192.168.84.63",
     },
     "camera": {
-        "position": [0.02, 0, 0.024], # [x,y,z] in m relative to middle of front axle (x: forward, y: right, z: up)
-        "orientation": [15, 0, 0], # [pitch,roll,yaw] in degrees
-        "resolution": [240, 320], # [height, width] in pixels
-        "fov": 120, # in degrees
+        "position": [0.0, -0.005, 0.04], # [x,y,z] in m relative to middle of front axle (x: forward, y: right, z: up)
+        "orientation": [22, 0, 0], # [pitch,roll,yaw] in degrees
+        "resolution": [128, 160], # [height, width] in pixels
+        "fov": 80, # in degrees
         "max_range": 0.5, # in meters
         "line_thickness": 6 # in pixels
     },
@@ -37,7 +37,7 @@ config = {
     }
 }
 
-env = gym.make("tinycarlo-realworld-v2", config=config, render_mode="human")
+env = gym.make("tinycarlo-v2", config=config, render_mode="human")
 env = CTESparseRewardWrapper(env, 0.01)
 env = CTETerminationWrapper(env, 0.07, number_of_steps=5)
 env = CrashTerminationWrapper(env)
